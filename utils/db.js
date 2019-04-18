b180eadf7778fffa05d11f606d56d889450595d8
Supporterino <lars@roth-kl.de>
2019-04-18 13:43:26 +0200

    Added https to the host

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
-var url = "mongodb://172.17.0.3:27017/";
+var url = "mongodb://172.17.0.5:27017/";
 var env = "Prod";
 
 exports.getUrl = () => {
-  return url;
+    return url;
 };
 
 exports.getEnv = () => {
-  return env;
+    return env;
 }
