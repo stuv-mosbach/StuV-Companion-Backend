3e95c5e78a747a24211a50b831d58da15f48e1f3
Supporterino <lars@roth-kl.de>
2019-06-12 21:09:13 +0200

    8090

--- backend.js
+++ backend.js
@@ -6,11 +6,11 @@ var mongoose = require('mongoose');
 var dbProvider = require('./utils/db');
 var cors = require('cors');
 var swaggerUI = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
+    swaggerDocument = require('./swagger.json');
 
 var dbUrl = dbProvider.getUrl() + dbProvider.getEnv();
 var app = express();
-var port = 8080;
+var port = 8090;
 
 scheduler.run();
 
@@ -26,5 +26,5 @@ app.use('/dash', agendash(scheduler.get()));
 app.use('/api', routes);
 
 app.listen(port, () => {
-  console.log("Backend running on port: " + port);
+    console.log("Backend running on port: " + port);
 });
